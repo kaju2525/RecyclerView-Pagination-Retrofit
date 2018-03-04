b98f26564cb1c6dfe55b8f564b27320144fad347
hata02525 <hata02525@gmail.com>
2018-03-05 04:14:26 +0530

    Pagination Recyclerview and Retrofit

---
+++ app/src/main/java/com/maps/network/ServiceGenerator.java
@@ -0,0 +1,36 @@
+package com.maps.network;
+
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+/**
+ * Created by sab99r
+ */
+public class ServiceGenerator {
+
+    public static <S> S createService(Class<S> serviceClass) {
+
+        OkHttpClient httpClient=new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        client.addInterceptor(loggingInterceptor);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.sab99r.com/demos/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client.build())
+                .build();
+
+        return retrofit.create(serviceClass);
+
+    }
+
+}
